f6924d92b41bc8f5d92be572c147a385b0508819
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-12-05 19:36:01 -0800

    Forgot to import one of the files.

--- after/plugin/after.lua
+++ after/plugin/after.lua
@@ -1,11 +1,11 @@
 if vim.g.vscode then
     return
 else
-    require("after.plugin.telescope").nvim_setup()
-    require("after.plugin.fugitive").nvim_setup()
-    require("after.plugin.undotree").nvim_setup()
-    require("after.plugin.harpoon").nvim_setup()
     require("after.plugin.filetree").nvim_setup()
+    require("after.plugin.fugitive").nvim_setup()
+    require("after.plugin.harpoon").nvim_setup()
     require("after.plugin.lsp").nvim_setup()
-    require("after.plugin.lsp").nvim_setup()
+    require("after.plugin.telescope").nvim_setup()
+    require("after.plugin.treesitter").nvim_setup()
+    require("after.plugin.undotree").nvim_setup()
 end
