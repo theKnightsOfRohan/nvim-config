ecaf487f5b3e6d5774f3872d4ff0fedc5c8ef1eb
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-12-07 18:14:34 -0800

    Organization

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,4 +4,5 @@ if not vim.g.vscode then
     -- Makes the background transparent
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.opt.termguicolors = true
 end
