028ca8219869e8e18f371f64905b7a2b113543ab
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-12-11 19:09:52 -0800

    Add extra git shortcuts and prepare harpoon

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
+vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+vim.keymap.set("n", "<leader>gB", ":Git branch")
+vim.keymap.set("n", "<leader>gp", ":Git pull<CR>")
--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({})
 
 vim.keymap.set("n", "<leader>h", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
