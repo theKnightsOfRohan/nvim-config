7c1778ec1844bbf8893b9051b63ce4817f554473
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-12-13 13:34:38 -0800

    Add keymap for goto-references

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -25,6 +25,7 @@ cmp.setup({
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>i", function() vim.lsp.buf.hover() end, opts)
     lsp.buffer_autoformat() -- On write
 end)
