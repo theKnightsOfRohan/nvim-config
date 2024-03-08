e2a81dda843c263790bf03453de0ddf90da2c710
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-03-07 19:16:34 -0800

    Set up some better utility keymaps

--- lua/normal/options.lua
+++ lua/normal/options.lua
@@ -1,6 +1,8 @@
 -- Vim command remaps
 vim.g.mapleader = ";"
+
 vim.api.nvim_command("command! Q quitall")
+vim.api.nvim_set_keymap("i", ";;", "<Esc>$a;", { noremap = true, silent = true })
 
 -- Window navigation
 vim.keymap.set("n", "<S-Up>", function()
@@ -17,13 +19,13 @@ vim.keymap.set("n", "<S-Right>", function()
 end)
 
 -- Copy highlighted selection
-vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true })
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
 
 -- Quick save
 vim.keymap.set("n", "<leader>s", function()
     vim.lsp.buf.format()
     vim.cmd.write()
-end, { noremap = true })
+end, { noremap = true, silent = true })
 
 -- Visual mode bracket surround
 -- I need this because the autoclose.nvim plugin is only for insert mode
@@ -38,10 +40,25 @@ local surrounders = {
 }
 
 for open, close in pairs(surrounders) do
-    vim.api.nvim_set_keymap("v", open, "c" .. open .. close .. "<Esc>hp", { noremap = true })
+    vim.api.nvim_set_keymap("v", open, "c" .. open .. close .. "<Esc>hp", { noremap = true, silent = true })
 end
 
-vim.api.nvim_set_keymap("v", "{", "c" .. "{}<Left><CR><Up><Esc>o<Esc>p>>h", { noremap = true })
+vim.api.nvim_set_keymap("v", "{", "c" .. "{}<Left><CR><Up><Esc>o<Esc>ph", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('TextYankPost', {}),
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
+
+vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- General settings
 vim.opt.number = true
@@ -55,3 +72,4 @@ vim.opt.fillchars = { eob = " " }
 vim.opt.scrolloff = 20
 vim.opt.timeoutlen = 500
 vim.opt.updatetime = 100
+vim.g.netrw_banner = 0
