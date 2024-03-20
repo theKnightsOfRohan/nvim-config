9f71e49c90a71264cfebf4639eb44a7370995fbd
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-03-20 13:40:28 -0700

    Add preliminary fugit2 config

---
+++ lua/plugins/fugit2.lua
@@ -0,0 +1,20 @@
+return {
+    -- "SuperBo/fugit2.nvim",
+    -- opts = {},
+    -- dependencies = {
+    --     "MunifTanjim/nui.nvim",
+    --     "nvim-tree/nvim-web-devicons",
+    --     "nvim-lua/plenary.nvim",
+    --     {
+    --         "chrisgrieser/nvim-tinygit",
+    --         dependencies = {
+    --             "stevearc/dressing.nvim"
+    --         }
+    --     },
+    --     "sindrets/diffview.nvim"
+    -- },
+    -- cmd = { "Fugit2", "Fugit2Graph" },
+    -- keys = {
+    --     { "<leader><S-g>", mode = "n", "<cmd>Fugit2<cr>" }
+    -- },
+}
